9dd2a52c32b53d31562dec9a7b98cd53875d49ba
miliardulo <97499327+miliardulo@users.noreply.github.com>
2023-04-23 00:32:39 +0900

    Time: 35 ms (46.02%), Space: 14 MB (15.38%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk:list[str] = list()
+        left:tuple[str] = ("(","{","[")
+        right:tuple[str] = (')','}',']')
+        for c in s:
+            if c in left:
+                stk.append(c)
+                continue
+            if len(stk)>0 and stk[-1]==left[right.index(c)]:
+                stk.pop()
+                continue
+            return False
+        return not stk
+            

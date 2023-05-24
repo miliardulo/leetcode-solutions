5626ca7493c6217a82b2f85f4779cd498b58e1c9
miliardulo <97499327+miliardulo@users.noreply.github.com>
2023-05-24 22:37:10 +0900

    Time: 94 ms (14.12%), Space: 16.5 MB (25.72%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,17 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        M:int=len(image)
-        N:int=len(image[0])
-        # visited:list[list[bool]]=[[False for _ in range(N)] for _ in range(M)]
-        queue:list[list[int]] = [[sr, sc]]
         tgt:int = image[sr][sc]
         if tgt==color:
             return image
+        queue:list[list[int]] = [[sr, sc]]
         DELTA:list[list[int]] = [[0,1],[1,0],[-1,0],[0,-1]]
+        M:int=len(image)
+        N:int=len(image[0])
         while queue:
             pos:list[int] = queue.pop(0)
             image[pos[0]][pos[1]]=color
-            # visited[pos[0]][pos[1]]=True
             for d in DELTA:
                 newpos:list[int]=[pos[0]+d[0],pos[1]+d[1]]
                 if not ((0<= newpos[0] <M) and (0<= newpos[1] <N)):

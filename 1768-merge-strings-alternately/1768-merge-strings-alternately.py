97035c8fae058d28eb1c1631da45498c8270cbff
miliardulo <97499327+miliardulo@users.noreply.github.com>
2023-05-31 18:34:22 +0900

    Time: 52 ms (8.86%), Space: 16.2 MB (25.09%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,13 +4,13 @@ class Solution:
         l2: int = len(word2)
         i1: int = 0
         i2: int = 0
-        slst: List[str] = []
+        mstr: str = ''
         while i1<l1 and i2<l2:
-            slst.append(word1[i1])
+            mstr+=word1[i1]
             i1+=1
-            slst.append(word2[i2])
+            mstr+=word2[i2]
             i2+=1
-        slst.append(word1[i1:])
-        slst.append(word2[i2:])
-        return ''.join(slst)
+        mstr+=word1[i1:]
+        mstr+=word2[i2:]
+        return mstr
             
